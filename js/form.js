3ee4d462b8c0bed165517f4d6de2a0e5b7894b78
Akram <akr232009@gmail.com>
2023-11-22 20:08:06 +0600

    edit

--- js/form.js
+++ js/form.js
@@ -23,3 +23,54 @@ window.addEventListener('keydown', function(event) {
     modal.style.display = "none";
   }
 });
+
+const highlightField = document.getElementById('highlightField');
+const nameField_1 = document.getElementById('nameField_1');
+const nameField_2 = document.getElementById('nameField_2');
+const nameField_3 = document.getElementById('nameField_3');
+const nameErrorMessage_1 = document.getElementById('nameError_1');
+const nameErrorMessage_2 = document.getElementById('nameError_2');
+const nameErrorMessage_3 = document.getElementById('nameError_3');
+const consentErrorMessage = document.getElementById('consentError');
+const consentText = document.getElementById('consentText');
+
+nameField_1.addEventListener('click', function() {
+  this.style.borderColor = '#EC1211';
+  nameErrorMessage_1.style.display = 'block';
+});
+
+nameField_2.addEventListener('click', function() {
+  this.style.borderColor = '#EC1211';
+  nameErrorMessage_2.style.display = 'block';
+});
+
+nameField_3.addEventListener('click', function() {
+  this.style.borderColor = '#EC1211';
+  nameErrorMessage_3.style.display = 'block';
+});
+
+// nameField_1.addEventListener('click', nameField_1);
+// nameField_2.addEventListener('click', nameField_2);
+// nameField_3.addEventListener('click', nameField_3);
+
+function checkAllFieldsClicked() {
+  return nameField_1.style.borderColor === '#EC1211' &&
+    nameField_2.style.borderColor === '#EC1211' &&
+    nameField_3.style.borderColor === '#EC1211';
+}
+
+function giveAnErrorMessage() {
+  this.style.borderColor = '#EC1211';
+  if (checkAllFieldsClicked()) {
+    consentErrorMessage.style.display = 'block';
+  }
+}
+
+// closeButton.addEventListener('click', function() {
+//   // Сброс состояния всех полей
+//   nameField.style.borderColor = '#F1F1F1';
+//   emailField.style.borderColor = '#F1F1F1';
+//   phoneField.style.borderColor = '#F1F1F1';
+//   consentErrorMessage.style.display = 'none';
+// });
+
